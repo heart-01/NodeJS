7e28ca0fded0bdae2fd912559bd36fb0a663de15
Siwat Jermwatthana <heart.siwat@hiveground.com>
2022-11-21 23:04:48 +0700

    Feat: Delete file env

--- nest/cmstock-nestjs-workshop/src/app.module.ts
+++ nest/cmstock-nestjs-workshop/src/app.module.ts
@@ -4,12 +4,10 @@ import { AppService } from './app.service';
 import { StockModule } from './stock/stock.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/typeorm.config';
-import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     StockModule,
-    ConfigModule.forRoot(),
     TypeOrmModule.forRoot(typeOrmConfig),
   ],
   controllers: [AppController],
--- nest/cmstock-nestjs-workshop/src/config/typeorm.config.ts
+++ nest/cmstock-nestjs-workshop/src/config/typeorm.config.ts
@@ -1,12 +1,12 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
-  type: 'postgres',        // type ที่จะใช้ orm connect กับ database ถ้าเป็น mysql จะชื่อว่า mysql
+  type: 'postgres', // type ที่จะใช้ orm connect กับ database ถ้าเป็น mysql จะชื่อว่า mysql
   host: 'localhost',
   port: 5432,
   username: 'postgres',
   password: 'password',
   database: 'cmstock',
-  autoLoadEntities: true,   // true เพื่อให้มัน auto load entity เพื่อให้พร้อมใช้งาน
-  synchronize: true,        // sync ข้อมูลตลอดเวลา
+  autoLoadEntities: true, // true เพื่อให้มัน auto load entity เพื่อให้พร้อมใช้งาน
+  synchronize: true, // sync ข้อมูลตลอดเวลา
 };
