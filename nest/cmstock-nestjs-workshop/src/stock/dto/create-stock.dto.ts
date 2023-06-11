6d7a61b5792bd3089881949acc38dc7a4e6c9ccc
Siwat Jermwatthana <s1104300051612@gmail.com>
2023-06-11 22:45:56 +0700

    [Fix]: Change dto validator

--- nest/cmstock-nestjs-workshop/src/stock/dto/create-stock.dto.ts
+++ nest/cmstock-nestjs-workshop/src/stock/dto/create-stock.dto.ts
@@ -1,21 +1,31 @@
-import { IsNotEmpty, IsNumber, MinLength } from 'class-validator';
+import {
+  IsNotEmpty,
+  IsNumber,
+  IsPositive,
+  IsString,
+  Max,
+  MaxLength,
+  Min,
+  MinLength,
+} from 'class-validator';
 
 export class CreateStockDto {
   @IsNotEmpty()
+  @IsString()
   @MinLength(3, {
     message: 'Name is too short',
   })
+  @MaxLength(300)
   name: string;
 
-  // @IsNotEmpty()
-  // @IsNumber({ maxDecimalPlaces: 2 })
-  // price: number;
   @IsNotEmpty()
-  price: string;
+  @IsNumber({ maxDecimalPlaces: 2 })
+  @IsPositive()
+  @Min(1)
+  @Max(9999)
+  price: number;
 
-  // @IsNotEmpty()
-  // @IsNumber({ maxDecimalPlaces: 0 })
-  // stock: number;
-  @IsNotEmpty()
-  stock: string;
+  @IsNumber({ maxDecimalPlaces: 0 })
+  @IsPositive()
+  stock: number;
 }
--- nest/cmstock-nestjs-workshop/src/stock/stock.controller.ts
+++ nest/cmstock-nestjs-workshop/src/stock/stock.controller.ts
@@ -13,9 +13,9 @@ export class StockController {
   @Post()
   @UseInterceptors(FileInterceptor('file', multerOptions)) // UseInterceptors จะเป็น middleware แล้วสามารถส่ง true หรือ fase เพื่อไปต่อ ส่วน FileInterceptor อนุญาติให้ส่งไฟล์เข้ามาที่ route ได้ โดยส่งชื่อ field ว่า file
   // UsePipes เป็น hook ใช้ในการ tranform ข้อมูล หรือ ใช้ในการ validate data ก่อนเข้า controller
-  // ValidationPipe ทำหน้าที่ verification การตรวสอบข้อมูลใน pipe
+  // ValidationPipe ทำหน้าที่ validate data DTO
   @UsePipes(ValidationPipe) // ตรวจสอบข้อมูลที่ส่งเข้ามาในท่อส่งข้อมูลในที่นี้จะตรวจสอบข้อมูลโดย validate กับ dto
-  @UsePipes(new ChangeStringCasePipe()) // เรียกใช้ custom pipe ที่เขียนขึ้นมาและ จะถูกเรียกใช้งานก่อน ValidationPipe เสมอ
+  @UsePipes(new ChangeStringCasePipe()) // เรียกใช้ custom pipe ที่เขียนขึ้นมาและ pipe ที่ cutiom จะถูกเรียกใช้งานก่อน ValidationPipe เสมอ
   addStock(
     @UploadedFile() file: Express.Multer.File,
     @Body() createStockDto: CreateStockDto, // @Body() createStockDto: CreateStockDto คือ รับข้อมูลที่ส่งเข้ามาจาก body ทั้งหมดจะต้องมี data ที่ตรงกับ rule dto ของ createStockDto ทั้งหมด
