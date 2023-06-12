2541abffc092d19b6926615e317e1ac81eefd5e8
Siwat Jermwatthana <s1104300051612@gmail.com>
2023-06-12 20:08:08 +0700

    [Comment]: Add comment in file main

--- nest/cmstock-nestjs-workshop/src/main.ts
+++ nest/cmstock-nestjs-workshop/src/main.ts
@@ -6,6 +6,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // initialize swagger: http://localhost:3000/api
   const config = new DocumentBuilder()
     .setTitle('Workshop API')
     .setDescription('Workshop API')
@@ -14,10 +15,13 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
+  // ValidationPipe เพื่อตรวจสอบและกำหนดกฏการตรวจสอบข้อมูลเข้าสู่แอปพลิเคชันของคุณในรูปแบบที่เป็นมาตรฐาน
+  // whitelist: true: การตั้งค่าค่านี้จะทำให้ ValidationPipe กรองและอนุญาตเฉพาะข้อมูลที่ถูกต้องเท่านั้นให้ผ่านไปยังระบบของแอปพลิเคชัน โดยจะตัดสิ่งที่ไม่ได้ระบุไว้ในโครงสร้างข้อมูลที่กำหนดไว้ในสกุลเงิน (เช่น JSON schema) ซึ่งจะช่วยในการป้องกันการส่งข้อมูลที่ไม่ถูกต้องหรือไม่ได้รับการต้องการเข้าสู่ระบบของคุณ
+  // forbidNonWhitelisted: true: ค่านี้จะตรวจสอบและปฏิเสธข้อมูลที่ไม่ได้ระบุไว้ในโครงสร้างข้อมูลที่กำหนดไว้ในสกุลเงิน กล่าวคือหากมีข้อมูลที่ไม่ได้ระบุไว้ในโครงสร้างข้อมูล หรือไม่ถูกต้องตามเงื่อนไขที่กำหนด แอปพลิเคชันของคุณจะปฏิเสธและไม่รับข้อมูลดังกล่าว
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
   );
-  app.enableCors();
+  app.enableCors(); // Open CORS
   await app.listen(3000);
 }
 bootstrap();
